f526d638543af0f64358afdcfacb2b5980c74038
Brayan Bedritchuk <brayan.bedritchuk@gmail.com>
2018-04-24 13:34:30 -0300

    Update tests

--- java-home-inventory-core/test/br/com/sailboat/homeinventory/core/interactor/validator/ProductValidatorTest.java
+++ java-home-inventory-core/test/br/com/sailboat/homeinventory/core/interactor/validator/ProductValidatorTest.java
@@ -1,5 +1,6 @@
 package br.com.sailboat.homeinventory.core.interactor.validator;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import br.com.sailboat.homeinventory.core.entity.Product;
@@ -7,57 +8,52 @@ import br.com.sailboat.homeinventory.core.exception.FieldNotFilledException;
 
 public class ProductValidatorTest {
 
+    private Product product;
+
+    @Before
+    public void createProduct() {
+        product = new Product();
+    }
+
     @Test(expected = FieldNotFilledException.class)
     public void testNameNull() throws FieldNotFilledException {
-        Product product = new Product();
         product.setName(null);
-        product.setQuantity(10);
-
-        ProductValidator.validate(product);
+        new ProductValidator(product).validateName();
     }
 
     @Test(expected = FieldNotFilledException.class)
     public void testNameEmpty() throws FieldNotFilledException {
-        Product product = new Product();
         product.setName("     ");
-        product.setQuantity(10);
+        new ProductValidator(product).validateName();
+    }
 
-        ProductValidator.validate(product);
+    @Test
+    public void testQuantityNull() throws FieldNotFilledException {
+        product.setQuantity(null);
+        new ProductValidator(product).validateQuantity();
     }
 
     @Test(expected = FieldNotFilledException.class)
     public void testQuantityNegative() throws FieldNotFilledException {
-        Product product = new Product();
-        product.setName("Product");
-        product.setQuantity(-10);
-
-        ProductValidator.validate(product);
+        product.setQuantity(-1);
+        new ProductValidator(product).validateQuantity();
     }
 
     @Test
     public void testNameFilled() throws FieldNotFilledException {
-        Product product = new Product();
         product.setName("Product");
-        product.setQuantity(0);
-
         new ProductValidator(product).validateName();
     }
 
     @Test
     public void testQuantityZero() throws FieldNotFilledException {
-        Product product = new Product();
-        product.setName("Product");
         product.setQuantity(0);
-
         new ProductValidator(product).validateQuantity();
     }
 
     @Test
     public void testQuantityTen() throws FieldNotFilledException {
-        Product product = new Product();
-        product.setName("Product");
         product.setQuantity(10);
-
         new ProductValidator(product).validateQuantity();
     }
 
